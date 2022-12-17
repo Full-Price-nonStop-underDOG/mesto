6bb03e8088077e18a9d67e7d831166c84a68e7d5
Дмитрий Рымарь <bogdanus@MacBook-Air-Dmitrij.local>
2022-12-17 14:05:24 +0300

    final

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 let buttonEdit = document.querySelector('.profile__button-edit');
 let form = document.querySelector('.popup');
 let formSubmit = document.querySelector('.form')
-let nameInput = document.querySelector('.form__input-name');
-let jobInput = document.querySelector('.form__input-job');
+let nameInput = document.querySelector('.form__input_text_name');
+let jobInput = document.querySelector('.form__input_text_job');
 let newName = document.querySelector('.profile__name');
 let newJob = document.querySelector('.profile__job');
 let buttonClose = document.querySelector('.popup__close');
@@ -11,15 +11,14 @@ let buttonClose = document.querySelector('.popup__close');
 function handleFormSubmit(evt) {
   evt.preventDefault();
 
-
-
   newName.textContent = nameInput.value;
   newJob.textContent = jobInput.value;
-  form.classList.remove('popup_open')
-
+  form.classList.remove('popup_open');
 }
 
 buttonEdit.addEventListener('click', () => {
+  nameInput.value = newName.textContent;
+  jobInput.value = newJob.textContent;
   form.classList.add('popup_open');
 });
 
